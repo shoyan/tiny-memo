4b9e81676fcfda195207c217549d3484fab03e7d
Shohei Yamasaki <yamasaki0406@gmail.com>
2019-12-16 18:05:53 +0900

    Refactoring

--- create.php
+++ create.php
@@ -1,29 +1,217 @@
-<?php 
-define('DB_HOST', 'localhost');
+<?php
+define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'memopad');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_PORT', '43306');
 
 // 文字化け対策
-$options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET CHARACTER SET 'utf8'");
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET 'utf8'");
 
 // PHPのエラーを表示するように設定
 error_reporting(E_ALL & ~E_NOTICE);
 
 // データベースの接続
 try {
-     $dbh = new PDO('mysql:host='.DB_HOST.';port=' . DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASSWORD, $options);
-     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbh = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD, $options);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-     echo $e->getMessage();
-     exit;
+  echo $e->getMessage();
+  exit;
+}
+
+/**
+ * メモを操作するクラス
+ */
+class Memo
+{
+  private $id;
+  private $title;
+  private $message;
+  private $createdAt;
+  private $updatedAt;
+
+  /**
+   * DBの接続情報を保持
+   */
+  private static $dbh;
+
+  /**
+   * DBの接続情報をセットする
+   */
+  public static function connect($dbh)
+  {
+    self::$dbh = $dbh;
+  }
+
+  function __construct($id, $title, $message, $createdAt, $updatedAt)
+  {
+    $this->id = $id;
+    $this->title = $title;
+    $this->message = $message;
+    $this->createdAt = $createdAt;
+    $this->updatedAt = $updatedAt;
+  }
+
+  public function id()
+  {
+    return $this->id;
+  }
+
+  public function title()
+  {
+    return $this->title;
+  }
+
+  public function setTitle($title)
+  {
+    return $this->title = $title;
+  }
+
+  public function message()
+  {
+    return $this->message;
+  }
+
+  public function setMessage($message)
+  {
+    return $this->message = $message;
+  }
+
+  public function createdAt()
+  {
+    return $this->createdAt;
+  }
+
+  public function updatedAt()
+  {
+    return $this->updatedAt;
+  }
+
+  /**
+   * 全てのメモを取得する
+   * 
+   * @return array 全てのメモ
+   */
+  public static function all()
+  {
+    $sql = 'SELECT * FROM memo';
+    $stmt = self::$dbh->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  /**
+   * メモを1件取得する
+   * 
+   * @param int メモID
+   * @return memo メモ情報
+   */
+  public static function findById($id)
+  {
+    $sql = 'SELECT * FROM memo WHERE id = ?';
+    $stmt = self::$dbh->prepare($sql);
+    $data[] = $id;
+    $stmt->execute($data);
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    return new Memo($rec['id'], $rec['title'], $rec['message'], $rec['createdAt'], $rec['updatedAt']);
+  }
+
+  /**
+   * メモを作成する
+   * 
+   * @param string タイトル
+   * @param string 内容
+   * @return Memo メモ情報
+   */
+  public static function create($title, $message)
+  {
+    $stmt = self::$dbh->prepare("INSERT INTO memo (title,message) VALUES (?,?)");
+    $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+    $stmt->bindParam(':message', $message, PDO::PARAM_STR);
+    $data[] = $title;
+    $data[] = $message;
+    $stmt->execute($data);
+    return Memo::findById(self::$dbh->lastInsertId());
+  }
+
+  /**
+   * メモを更新する
+   * 
+   * @return boolean 成功時はtrue
+   */
+  public function update()
+  {
+    $sql = 'UPDATE memo SET title=?,message=? WHERE id=?';
+    $stmt = self::$dbh->prepare($sql);
+    $data[] = $this->title;
+    $data[] = $this->message;
+    $data[] = $this->id;
+    return $stmt->execute($data);
+  }
+
+  /**
+   * メモを削除する
+   * 
+   * @return boolean 成功時はtrue
+   */
+  public function delete()
+  {
+    $sql = 'DELETE FROM memo WHERE id=?';
+    $stmt = self::$dbh->prepare($sql);
+    $data[] = $this->id;
+    return $stmt->execute($data);
+  }
+}
+
+// 接続情報を設定する
+Memo::connect($dbh);
+
+
+if (isset($_POST["junction"])) {
+  $row = $_POST["junction"];
+
+  switch ($row) {
+    case "更新":
+      if (empty($_POST['id'])) {
+        print 'idを入力してください!';
+      } else {
+        $memo = Memo::findById($_POST['id']);
+        $memo->setTitle($_POST['title']);
+        $memo->setMessage($_POST['message']);
+        $memo->update();
+      }
+      break;
+
+    case "送信":
+      $memo = Memo::create($_POST['title'], $_POST['message']);
+     break;
+
+    case "削除":
+      $memo = Memo::findById($_POST['id']);
+      $memo->delete();
+      // メモ作成後はメモはリダイレクトする
+      header('Location: /memopad/create.php');
+      exit;
+  }
+} elseif (!empty($_GET['linkId'])) {
+  $memo = Memo::findById($_GET['linkId']);
+}
+
+$id = '';
+$title = '';
+$message = '';
+
+if ($memo) {
+  $id = $memo->id();
+  $title = $memo->title();
+  $message = $memo->message();
 }
 
 ?>
-
-
+<!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <title>メモ帳</title>
@@ -31,114 +219,34 @@ try {
 </head>
 
 <body>
-<?php
-if (isset($_POST["junction"])) {
-    $row = $_POST["junction"];
+  <form method='post' action='/memopad/create.php'>
+    <div class="sidebar">
+      <p>メモ一覧<p>
+      <br />
+      <a href='/memopad/create.php'><input name='back' type='submit' value='新規作成'></a>
 
-    switch ($row) {
-        case "更新": 
-          if (empty($_POST['id'])) {
-            print 'idを入力してください!';
-          } else {
-          $id = $_POST['id'];
-          $title = $_POST['title'];
-          $message = $_POST['message'];
-          
-          $sql = 'UPDATE memo SET title=?,message=? WHERE id=?';
-          $stmt = $dbh->prepare($sql);
-          $data[] = $title;
-          $data[] = $message;
-          $data[] = $id;
-          $stmt->execute($data);
-          }
-        break;
+      <?PHP foreach (Memo::all() as $row) : ?>
+        <p class='dot'>id:</p>
+        <a href='/memopad/create.php?linkId=<?PHP print $row["id"] ?>'><input id='linkId' class='linkId' type='button' name='linkId' value='<?PHP print $row["id"] ?>'></a>
+        <br>
+        <p><?PHP print $row['title'] ?></p>
+        <p class="overflow-ellipsis"><?PHP print $row['message'] ?></p>
+        <br />
+      <?PHP endforeach ?>
+    </div>
 
-        case "送信":
-          $title = $_POST['title'];
-          $message = $_POST['message'];
-    
-          $stmt = $dbh->prepare("INSERT INTO memo (title,message) VALUES (?,?)");
-          $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_STR);
-          $stmt->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
-          $stmt->bindParam(':message', $_POST['message'], PDO::PARAM_STR);
-          $data[] = $title;
-          $data[] = $message;
-    
-          $stmt->execute($data);
-        break;
-
-        case "削除": 
-          $id = $_POST['id'];
-          $sql = 'DELETE FROM memo WHERE id=?';
-          $stmt = $dbh->prepare($sql);
-          $data[] = $id;
-          $stmt->execute($data);
-        break;
-    }
-}
-?>
-
-<form method='post' action='/memopad/create.php'>
-        
-        <div class="sidebar">
-          <p>メモ一覧<p>
-          <br/>
-          <a href='http://localhost/memopad/create.php'><input name='back' type='submit' value='新規作成'></a>
-          <?php
-            $sql = 'SELECT id,title,message FROM memo WHERE 1';
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute();
-          ?>
-            
-    
-          <?PHP  while($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-              <p class='dot'>id:</p>
-              <a href='http://localhost/memopad/create.php?linkId=<?PHP print $row["id"] ?>'><input id='linkId' class='linkId' type='button' name='linkId' value='<?PHP print $row["id"] ?>'></a>
-              <br>
-              <p><?PHP print $row['title'] ?></p>
-              <p class="overflow-ellipsis"><?PHP print $row['message'] ?></p>
-              <br/>
-          <?PHP endwhile ?>
-          
-          <?php
-            if (!empty($_GET['linkId'])) {
-
-            $id = $_GET['linkId'];
-      
-            $sql = 'SELECT title,message FROM memo WHERE id=?';
-            $stmt = $dbh->prepare($sql);
-            $data[] = $id;
-            $stmt->execute($data);
-
-            $rec=$stmt->fetch(PDO::FETCH_ASSOC);
-            $title=$rec['title'];
-            $message=$rec['message'];
-            }
-
-          ?>
-
-        </div>
-
-      <div class="main">
-        <input id='id' type='text' name='id' class='id' value='<?PHP print $id ?>' placeholder="id">
-        <input id='title' type='text' name='title' class='title' value='<?PHP print $title?>' placeholder="件名">
-        <div><textarea id='message' name='message' class='message'><?PHP print $message ?></textarea></div>
-        <div class= "bottun">
-          <input type='submit' name='junction' value='送信'>
-          <input type="submit" name="junction" value="更新">
-          <input class="red" type="submit" name="junction" value="削除"><br/>
-        </div>
-        <br/>
-        <br/>
+    <div class="main">
+      <input id='id' type='text' name='id' class='id' value='<?PHP print $id ?>' placeholder="id">
+      <input id='title' type='text' name='title' class='title' value='<?PHP print $title ?>' placeholder="件名">
+      <div><textarea id='message' name='message' class='message'><?PHP print $message ?></textarea></div>
+      <div class="bottun">
+        <input type='submit' name='junction' value='送信'>
+        <input type="submit" name="junction" value="更新">
+        <input class="red" type="submit" name="junction" value="削除"><br />
       </div>
-    </form>
-
-
-
-
-    <?php  $dbh = null; ?>
-
-
-
+      <br />
+      <br />
+    </div>
+  </form>
 </body>
 </html>
